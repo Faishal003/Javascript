100831a9c2705415dcdd7c6a71dbc66b7575dbce
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-28 23:56:31 +0600

    JS (passing data to EJS)

---
+++ ejsDir/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 8080;
+
+app.set("view engine", 'ejs');
+app.set("views", path.join(__dirname, '/views'));
+
+app.listen(port, ()=>{
+    console.log(`app is on listening: ${port}`);
+})
+
+app.get('/', (req, res)=>{
+    res.render("home.ejs");
+})
+
+app.get('/rolldice', (req, res)=>{
+    let diceVal = Math.floor(Math.random() * 6) + 1;
+    res.render("rollDice.ejs", {diceVal});
+})
