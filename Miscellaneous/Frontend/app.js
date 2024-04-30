33cf0606574eac11a55dfdb27d988ad5fc247ac2
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-04-30 08:31:25 +0600

    Miscellaneous (inheritance)

--- Miscellaneous/Frontend/app.js
+++ Miscellaneous/Frontend/app.js
@@ -1,35 +1,4 @@
-//factory method
-
-// function PersonMaker(name, age){
-//     const person = {
-//         name: name,
-//         age: age,
-//         talk(){
-//             console.log(`Hi, my name is ${this.name}`);
-//         },
-//     };
-
-//     return person;
-// }
-
-//problem is create a different object using factory method...
-
-
-
-//Constructor - does not return anything and start with capital
-//thats the better way to handle objects
-// function Person(name, age){
-//     this.name = name;
-//     this.age = age;
-// }
-
-// Person.prototype.talk = function(){
-//     console.log(`Hi, my name is ${this.name}`);
-// }
-
-
-//best way is Classes
-
+//inheritance
 class Person{
     constructor(name, age){
         this.name = name;
@@ -37,8 +6,22 @@ class Person{
     }
     talk(){
         console.log(`hi, my name is ${this.name}`);
-    };
+    }
 }
 
-let p1 = new Person("faishal", 25);
-let p2 = new Person("ahmed", 27); 
+class Student extends Person{
+    constructor(name, age, marks){
+        super(name, age) // parent class constructor is being called
+        this.marks = marks;
+    }
+}
+
+class Teacher extends Person{
+    constructor(name, age, subject){
+        super(name, age);
+        this.subject = subject;
+    }
+}
+
+let s1 = new Student("faishal", 21,100);
+let t1 = new Teacher("Ahmed emon", 30, "CSE");
