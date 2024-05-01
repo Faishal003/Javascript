4cc8ad512c488bd8a992e5fd905a5a3baa0c1c24
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-05-01 10:02:58 +0600

    Rest_Class (index route)

--- Rest_Class/index.js
+++ Rest_Class/index.js
@@ -4,13 +4,29 @@ const path = require("path")
 const port = 8080;
 
 app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res)=>{
-    res.send("all working well");
+let posts = [
+    {
+        username: "faishal ahmed emon",
+        content: "This is all about programming.",
+    },
+    {
+        username: "Captain america",
+        content: "I can do this all day long.",
+    },
+    {
+        username: "bubin",
+        content: "Arise a bug in that banking project.",
+    }
+]
+
+
+app.get('/posts', (req, res)=>{
+    res.render("index.ejs", {posts});
 })
 
 app.listen(port, ()=>{
