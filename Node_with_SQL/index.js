c58dacbe2653644f30e49e3a3f01daa5963433cd
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-05-05 08:09:56 +0600

    Node_With_SQL (update the specific user's username)

--- Node_with_SQL/index.js
+++ Node_with_SQL/index.js
@@ -2,6 +2,14 @@ const { faker } = require('@faker-js/faker');
 const mysql = require("mysql2");
 const express = require("express");
 const app = express();
+const path = require("path");
+const methodOverride = require('method-override')
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended:true}));
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -19,6 +27,7 @@ let getRandomUser = ()=> {
     ]
   };
 
+//homepage
 app.get('/', (req, res)=>{
     let q = `SELECT COUNT(*) FROM user`;
     try {
@@ -26,7 +35,8 @@ app.get('/', (req, res)=>{
       if(err) throw err;
       console.log(result[0]["COUNT(*)"]);
       let val = result[0]["COUNT(*)"]
-      res.send(`Total number of user is: ${val.toString()}`);
+    //   res.send(`Total number of user is: ${val.toString()}`);
+    res.render("home.ejs", {val});
     });
   } catch (err) {
     console.log(err);
@@ -34,9 +44,72 @@ app.get('/', (req, res)=>{
   }
 });
 
+//fetch user table all info
+app.get("/users", (req, res)=>{
+    let q = `SELECT * FROM user`;
+    try {
+        connection.query(q, (err, users)=>{
+          if(err) throw err;
+          res.render("fetchuser.ejs", {users});
+        });
+      } catch (err) {
+        console.log(err);
+        res.send("Error Occured..");
+      }
+});
+
+//edit user information
+app.get("/users/:id/edit", (req, res)=>{
+    let {id} = req.params;
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+    try {
+        connection.query(q, (err, result)=>{
+          if(err) throw err;
+          let user = result[0];
+          res.render("edituser.ejs", {user});
+        });
+      } catch (err) {
+        console.log(err);
+        res.send("Error Occured..");
+      }
+});
+
+//update (db) 
+app.patch("/users/:id", (req, res)=>{
+    let {id} = req.params;
+    let {password: formPassword, username: newUsername} = req.body;
+
+    let q = `SELECT * FROM user WHERE id='${id}'`;
+    try {
+        connection.query(q, (err, result)=>{
+          if(err) throw err;
+          let user = result[0];
+
+          if(formPassword != user.password){
+            res.send("WRONG PASSWORD");
+          }else{
+            let q2 = `UPDATE user SET username='${newUsername}' WHERE id='${id}'`;
+            connection.query(q2, (err, result)=>{
+                if(err) throw err;
+                res.redirect("/users");
+            });
+          }
+        });
+      } catch (err) {
+        console.log(err);
+        res.send("Error Occured..");
+      }
+})
+
 app.listen("8080", ()=>{
     console.log(`Listening to the port: 8080`);
 })
+
+// let q = "INSERT INTO user (id, username, email, password) VALUES ?";
+// let data = [];
+// for(i = 0; i < 100; i++){
+//     data.push(getRandomUser());
+// }
 
 // try {
 //     connection.query(q, [data], (err, result)=>{
