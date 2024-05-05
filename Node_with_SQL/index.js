c2c4f40f29f0d33168471f80df128e2eec6b5fd3
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-05-05 06:12:18 +0600

    Node_With_SQL (added 202 username to the homepage with get req)

--- Node_with_SQL/index.js
+++ Node_with_SQL/index.js
@@ -1,5 +1,7 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require("mysql2");
+const express = require("express");
+const app = express();
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -17,21 +19,32 @@ let getRandomUser = ()=> {
     ]
   };
 
-//Inserting New data
-let q = "INSERT INTO user (id, username, email, password) VALUES ?";
-let data = [];
-for(i = 0; i < 100; i++){
-    data.push(getRandomUser());
-}
+app.get('/', (req, res)=>{
+    let q = `SELECT COUNT(*) FROM user`;
+    try {
+    connection.query(q, (err, result)=>{
+      if(err) throw err;
+      console.log(result[0]["COUNT(*)"]);
+      let val = result[0]["COUNT(*)"]
+      res.send(`Total number of user is: ${val.toString()}`);
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("Error Occured..");
+  }
+});
 
-try {
-  connection.query(q, [data], (err, result)=>{
-    if(err) throw err;
-    console.log(result);
-    console.log(result.length);
-  });
-} catch (err) {
-  console.log(err);
-}
+app.listen("8080", ()=>{
+    console.log(`Listening to the port: 8080`);
+})
 
-connection.end();
+// try {
+//     connection.query(q, [data], (err, result)=>{
+//       if(err) throw err;
+//       console.log(result);
+//       console.log(result.length);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//   }
+// connection.end();
