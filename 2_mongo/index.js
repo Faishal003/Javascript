a2262da5660ccb34ddafe91b28cfceaedcd93289
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-05-09 03:10:06 +0600

    mongo (methods like find,findone,findbyid)

--- 2_mongo/index.js
+++ 2_mongo/index.js
@@ -16,18 +16,14 @@ const userSchema = new mongoose.Schema({
     age: String
 })
 
+// const User = mongoose.model("User", userSchema);
+
+// User.find({age: {$gt: 30}})
+// .then((res)=>console.log(res))
+// .catch((err)=>console.log(err))
+
 const User = mongoose.model("User", userSchema);
 
-// const user1 = new User({
-//   name: "faishal ahmed emon",
-//   email: "faishal@gmail.com",
-//   age: "20"
-// });
-
-// user1.save();
-
-User.insertMany([
-  {name: "Tony", email: "tony@gmail.com", age: 50},
-  {name: "hulk", email: "hulk@gmail.com", age: 55},
-  {name: "Peter", email: "peter@gmail.com", age: 26},
-]).then((res)=>console.log(res));
+User.findById("663be5f8b9bbf3d52c7ea616")
+.then((res)=>console.log(res))
+.catch((err)=>console.log(err))
