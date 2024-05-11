8ad8711e1b6f06b8b961edec84fe244cad32ec6f
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-05-11 20:15:02 +0600

    3_mongo (creating our model)

--- 3_mongo/index.js
+++ 3_mongo/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -13,6 +14,14 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/chat');
 }
+
+let chat1 = new Chat({
+  from: "tony",
+  to: "hulk",
+  msg: "hi, big man",
+  created_at: new Date(),
+});
+chat1.save().then((res)=>console.log(res));
 
 app.get('/', (req, res)=>{
     res.send("welcome to homepage.")
