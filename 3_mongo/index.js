1bf9bd289d63014695a62d6877badccc13cdfef6
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-05-15 02:15:25 +0600

    3_mongo (create route)

--- 3_mongo/index.js
+++ 3_mongo/index.js
@@ -6,7 +6,8 @@ const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
 
 main()
 .then(()=> console.log("connection successfull"))
@@ -25,6 +26,18 @@ app.get('/chats', async (req, res)=>{
 //new chat
 app.get('/chats/new', (req, res)=>{
   res.render("new.ejs");
+});
+
+app.post('/chats', (req, res)=>{
+  let {from, to, msg} = req.body;
+  let chats = new Chat({
+    from: from,
+    to: to,
+    msg: msg,
+    created_at: new Date(),
+  });
+  chats.save().then((res)=>console.log("chat was saved")).catch((err)=>console.log(err));
+  res.redirect("/chats");
 });
 
 app.get('/', (req, res)=>{
