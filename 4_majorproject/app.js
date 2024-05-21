e4488f511a62a68e4ea96f238774d2cfdb081337
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-05-21 15:41:11 +0600

    3_majorproject (listing model)

--- 4_majorproject/app.js
+++ 4_majorproject/app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Listing = require('./models/listing.js');
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 main()
-.then(()=> console.log("connection successfull"))
+.then(()=> console.log("connection successfull to the database"))
 .catch(err => console.log(err));
 
 async function main() {
@@ -17,6 +18,21 @@ async function main() {
 app.get('/', (req, res)=>{
     res.send("welcome to homepage.")
 });
+
+//sample testing listing
+app.get('/testlisting', async (req, res)=>{
+  let sampleListing = new Listing({
+    title: "My new Villa",
+    description: "by the beach",
+    price: 5000,
+    location: "COX's Bazar",
+    country: "Bangladesh",
+  });
+
+  await sampleListing.save();
+  console.log("sample was saved");
+  res.send("testing successfull")
+})
 
 app.listen(8080, (req, res)=>{
     console.log("server is listening on port 8080: ")
--- 4_majorproject/models/listing.js
+++ 4_majorproject/models/listing.js
@@ -6,7 +6,16 @@ const listingSchema = new Schema({
         type: String,
         required: true,
     },
-    description: {
-        
-    }
-})
+    description: String,
+    image: {
+        type: String,
+        default: "https://unsplash.com/photos/the-sun-is-setting-over-the-water-at-the-beach-PG1NrnwhOnU",
+        set: (v) => v === "" ? "https://unsplash.com/photos/the-sun-is-setting-over-the-water-at-the-beach-PG1NrnwhOnU" : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
