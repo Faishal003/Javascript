030ec6ee78288542cb3114d7869f78834d2d4402
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-05-21 15:14:15 +0600

    3_majorproject (initialization)

---
+++ 4_majorproject/models/listing.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        
+    }
+})
