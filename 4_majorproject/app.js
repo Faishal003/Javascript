75831b3ba78d53a64ac12ad94ba7bc1d09c8b148
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-05-24 04:13:58 +0600

    4_MajorProject(show route)

--- 4_majorproject/app.js
+++ 4_majorproject/app.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Listing = require('./models/listing.js');
 
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -23,7 +26,14 @@ app.get('/', (req, res)=>{
 app.get('/listing', async (req, res)=>{
   const allListing = await Listing.find({});
   res.render("listing/index.ejs", {allListing});
-})
+});
+
+//
+app.get('/listing/:id', async (req, res)=>{
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listing/show.ejs", {listing});
+});
 
 app.listen(8080, (req, res)=>{
     console.log("server is listening on port 8080: ")
