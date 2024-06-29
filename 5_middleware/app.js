c64f76a6f932c725644970c5dfbb10caff8aceb3
MD. Faishal Ahmed <73869161+Faishal003@users.noreply.github.com>
2024-06-29 09:25:35 +0600

    5_middleware{exploring app.use()}

--- 5_middleware/app.js
+++ 5_middleware/app.js
@@ -16,6 +16,11 @@ app.get("/random", (req, res)=>{
     res.send("This is the random page.");
 });
 
+//404 not found
+app.use((req, res)=>{
+    res.send("Page not found");
+})
+
 app.listen(8080, ()=>{
     console.log("Server is listening to the port 8080");
 });
